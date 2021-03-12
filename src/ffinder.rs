57f0bbd57a55522ba5c6c08be398940ed80adb7d
speltriao <38846168+speltriao@users.noreply.github.com>
2021-03-12 15:20:39 -0300

    Add files via upload

--- src/ffinder.rs
+++ src/ffinder.rs
@@ -3,14 +3,12 @@ use std::mem;
 use colored::*;
 use std::process::exit;
 
-
 struct Input{
     prmtr: String, //parameter being searched
     base_dir: String, //Base directory to start search
     extension:bool, //user included the file extension or not (.something)//
     hidden:bool, //search or not in hidden directories
 }
-
 
 fn input()->Input{
     let mut args: Vec<String> = env::args().collect();
@@ -42,7 +40,6 @@ fn input()->Input{
     }
     if p.len()==0{
         println!("{}","Please specify a paramater!".red().bold().italic());
-        //println!("{} {} {}", "or use".cyan(), "any".italic().yellow(), "string type".cyan());
         exit(0);
     }
     let inp = Input {
@@ -88,6 +85,18 @@ fn get_home()-> String{
     s
 }
 
+fn rmv_extension(stri: String) -> String{
+    let mut s = stri.clone();
+    let mut i = 0;
+    //println!("{}{}",s.len(),s.len()-1);
+    if s.chars().last().unwrap()=='/'{
+        s.pop();
+    }
+    if let Some(n) = s.rfind("."){
+        i = n;
+    }
+    s[..i].to_string()
+}
 
 fn vectorize(stri: String,ext:bool) -> Vec<String> { //create a vector in which every element is a single word from string
     let mut vec: Vec<String> = Vec::new();
@@ -111,16 +120,19 @@ fn vectorize(stri: String,ext:bool) -> Vec<String> { //create a vector in which 
         mem::forget(s);
         
     }
-    //println!("Vec {:?}",vec);
     vec
 } 
+
 
 fn string_to_static_str(s: String) -> &'static str {
     Box::leak(s.into_boxed_str())
 }
 
-fn compare(current:String,prmtr:String,  ext:bool) -> bool {
-    if !current.contains(" ") { //Current file is compound of a single word
+fn compare(mut current:String,prmtr:String,  ext:bool) -> bool {
+    if !ext { //If user didn't specified extension, remove it from file being compared
+        current = rmv_extension(current);
+    }
+    if !prmtr.contains(" ") { //Current file is compound of a single word
         if prmtr == current {
             return true;
         }
@@ -128,26 +140,15 @@ fn compare(current:String,prmtr:String,  ext:bool) -> bool {
             return false;
         }
     }
+
     let (p,mut c) = (vectorize(prmtr,ext),vectorize(current,ext));
     if ext && p[p.len()-1]!=c[c.len()-1]{//user gave extension, and the current extension doesn't match.
         return false;
     }
-    if !ext{
-        let size = c.len()-1;
-        let s = c[size].clone();
-        match s.find(".") {
-            Some(n) => {
-                let slice:String = s[..n].to_string();
-                c[size]=slice;
-            }
-            None => (),
-        }
 
-        mem::forget(s);
-    }
     let (h, mut count):(f32, u8) = ((c.len() as f32 / 2.0).round(), 0);
     let half = h as u8;
-    //println!("Current {:?}",c);
+    
     for s1 in c {
         for s2 in &p {
             if s1.to_uppercase()==s2.to_uppercase(){ //case insensitive comparison
@@ -164,7 +165,7 @@ fn compare(current:String,prmtr:String,  ext:bool) -> bool {
 }
 
 
-fn ffinder(base_dir:String, prmtr:&str, e:bool, h:bool) -> std::io::Result<()>{ // prmtr:String
+fn ffinder(base_dir:String, prmtr:&'static str, e:bool, h:bool) -> std::io::Result<()>{ // prmtr:String
     let pth = std::fs::read_dir(&base_dir)?;
         
     for p in pth {
@@ -177,22 +178,24 @@ fn ffinder(base_dir:String, prmtr:&str, e:bool, h:bool) -> std::io::Result<()>{ 
                     ffinder(p2.display().to_string(),prmtr,e,h);
                 }
             }
-            else {
+            else {//search include hidden directories
                 ffinder(p2.display().to_string(),prmtr,e,h);
-            } //search include hidden directories
+            } 
         }
         else {
             if compare(rmv_underline(get_fname(p2.display().to_string())),rmv_underline(prmtr.to_string()),e){
-                println!("File found at: {}",p2.display().to_string().blue());
+                    if compare(rmv_underline(get_fname(p2.display().to_string())),rmv_underline(prmtr.to_string()),e){
+                        println!("File found at: {}",p2.display().to_string().blue());
+                    }
+
             }
         }
     }
     Ok(())
 }
 
-
 fn main(){
     let i = input();
-    ffinder(i.base_dir,string_to_static_str(i.prmtr),i.extension,i.hidden);
+    ffinder(i.base_dir,string_to_static_str(i.prmtr),i.extension,i.hidden);    
 }
 
